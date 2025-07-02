d3dedb951bcfdaa7b871b29836850b3a26de060f
delta <ahmaisma755@gmail.com>
2025-07-02 03:21:14 +0300

    ijre

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+const request = require ("request");
+const https = require("https");
+const app=express();
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.listen (process.env.PORT || 3000,function(){
+    console.log("server is running");
+})
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signin.html");
+})
+
+app.post("/signin",function(req,res){
+    var fname=req.body.fname;
+    var lname=req.body.lname;
+    var email=req.body.email;
+    var data={
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:fname,
+                    LNAME:lname
+                }
+            }
+        ]
+    };
+    var jsonData=JSON.stringify(data);
+    const url="https://us3.api.mailchimp.com/3.0/lists/c7f48a5049";
+    const options={
+        method:"POST",
+        auth:"user:813ba42d0cb1d667ad876188b1e896b-us3"
+    };
+    const request = https.request(url,options,function(response){
+        if (response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+           response.on("data",function(data){
+            console.log(JSON.parse(data));
+           });
+        } else {
+            res.sendFile(__dirname+"/failure.html");
+        }
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure",function(req,res){
+    // Redirect to the signup page on failure
+    res.redirect("/");      
+})
+
+
+
+//8813ba42d0cb1d667ad876188b1e896b-us3 
+// c7f48a5049 

15bf3701ddeb0938c5bccd02e252658478b6b149
delta <ahmaisma755@gmail.com>
2025-07-03 15:10:32 +0300

    update the api of mailchip with securty

--- app.js
+++ app.js
@@ -31,12 +31,17 @@ app.post("/signin",function(req,res){
             }
         ]
     };
+    require('dotenv').config(); // Loads .env
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY, // Safely read from .env
+  server: process.env.MAILCHIMP_SERVER_PREFIX,
+});
     var jsonData=JSON.stringify(data);
     const url="https://us3.api.mailchimp.com/3.0/lists/c7f48a5049";
-    const options={
-        method:"POST",
-        auth:"user:e4b1a77426629e66fbf835178495321e-us3"
-    };
+   const options = {
+  method: "POST",
+  auth: `user:${process.env.MAILCHIMP_API_KEY}`
+};
     const request = https.request(url,options,function(response){
         if (response.statusCode===200){
             res.sendFile(__dirname+"/success.html");
@@ -59,6 +64,5 @@ app.post("/failure",function(req,res){
 
 
 
-//6072e2e05181249d279f70bfafd751fc-us3
+//11aac60c7db6c9837ea07e9e52118756-us3
 // c7f48a5049 
-//e4b1a77426629e66fbf835178495321e-us3
